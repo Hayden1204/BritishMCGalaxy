b2c3805e192715e184f9d447ab80e7718fda6ffd
Hayden Thirties <106928985+Hayden1204@users.noreply.github.com>
2026-08-22 00:54:04 +0100

    change software name from MCGalaxy to British MCGalaxy

--- MCGalaxy/Server/Server.Fields.cs
+++ MCGalaxy/Server/Server.Fields.cs
@@ -43,7 +43,7 @@ namespace MCGalaxy
         public const string InternalVersion = "1.9.5.3";
         public static string Version { get { return InternalVersion; } }
         
-        public static string SoftwareName = "MCGalaxy";
+        public static string SoftwareName = "British MCGalaxy";
         static string fullName;
         public static string SoftwareNameVersioned {
             // By default, if SoftwareName gets externally changed, that is reflected in SoftwareNameVersioned too
